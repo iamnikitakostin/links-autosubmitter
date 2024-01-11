c4879f735463ffe3e21d3a9c73009b26711b8e17
iamnikitakostin <141674557+iamnikitakostin@users.noreply.github.com>
2024-01-11 09:42:10 -0800

    stable version, fixed bugs

--- linksresubmission.js
+++ linksresubmission.js
@@ -16,145 +16,130 @@ async function linksResubmission() {
         }
 
         const urls = URLs.split("\n");
-        const urlListTrimmed = [];
-        urls.forEach((element) => {
-            const trimmed = element.trim();
-            urlListTrimmed.push(trimmed);
-        })
-        const urlList = urlListTrimmed.filter((element) =>
-            element.length > 2
-        )
+        const urlListTrimmed = urls.map(element => element.trim()).filter(element => element.length > 2);
         const submittedLinks = [];
         const failedLinks = [];
 
-        async function removeUrlJs(index, urlList, nextButton = false) {
+        async function removeUrlJs(index, urlList, nextButton = false, submitButtonFound = false) {
             if (index < urlList.length) {
-                if (!nextButton) {
-                    document.querySelector('.RveJvd.snByac').click();
-                }
+                await clickNextButton(nextButton);
+                setTimeout(async() => {
+                    await urlToSubmissionBar(urlList, index);
+                    setTimeout(async() => {
+                        await submissionNextButton();
+                        setTimeout(async() => {
+                            submitButtonFound = await submitRequest(submitButtonFound);
+                            setTimeout(async() => {
+                                await checkOutcome(urlList, index, submitButtonFound);
+                                setTimeout(async() => {
+                                    await removeUrlJs(index + 1, urlList, nextButton = false);
+                                }, 1500);
+                            }, 2000)
+                        }, 500);
+                    }, 500);
 
-                async function urlToSubmissionBar() {
-                    const urlBarLabelIndex = 0;
-                    const urlBarIndex = 0;
-                    const urlBarTextboxIndex = 1;
-                    const urlBarLabel = document.querySelectorAll('.Ufn6O.PPB5Hf')[urlBarLabelIndex];
-                    if (urlBarLabel) {
-                        const urlBar = urlBarLabel.childNodes[urlBarIndex].childNodes[urlBarTextboxIndex];
-                        if (urlBar) {
-                            urlBar.value = urlList[index];
-                        }
-                    }
-                    submissionNextButton();
-                }
-
-                async function submissionNextButton() {
-                    await urlToSubmissionBar();
-                    const nextButton = document.querySelectorAll('.RDPZE');
-                    const nextButtonSpan = 2;
-                    for (let j = 0; j < nextButton.length; j++) {
-                        if (nextButton[j].childNodes[nextButtonSpan]) {
-                            nextButton[j].removeAttribute('aria-disabled');
-                            nextButton[j].setAttribute('tabindex', 0);
-                            nextButton[j].childNodes[nextButtonSpan].click();
-                        }
-                    }
-                }
-                async function submitRequest() {
-                    await urlToSubmissionBar();
-                    let submitButtonFound = false;
-                    let closeButtonFound = false;
-                    const submitButton = document.querySelectorAll('.CwaK9 .RveJvd.snByac');
-
-                    for (let k = 0; k < submitButton.length; k++) {
-                        console.log(submitButton)
-                        console.log(k, closeButtonFound)
-                        if (submitButton[k].textContent.toLowerCase() == 'submit request') {
-                            submitButton[k].click();
-                            submitButtonFound = true;
-                            console.log("breaking out")
-                            break;
-                        }
-                        else {
-                            closeButtonFound = submitButton[k].textContent.toLowerCase() == 'close' ? true : false;
-                            if (closeButtonFound) {
-                                break;
-                            }
-                        }
-                    checkOutcome();
-                    }
-                        async function checkOutcome() {
-                            await submitRequest();
-                            if (document.querySelectorAll('.PNenzf').length > 0) {
-                                failedLinks.push(urlList[index]);
-                                console.log(`Failed, ${urlList[index]} already exists as a submitted request, duplicates are not permitted.`)
-                                const closeButton = document.querySelectorAll('.CwaK9 .RveJvd.snByac');
-                                for (let k = 0; k < closeButton.length; k++) {
-                                    if ((closeButton[k].childNodes[0] && (closeButton[k].childNodes[0].textContent).toLowerCase() == 'close') || (closeButton[2].textContent.toLowerCase() == 'close')) {
-                                        closeButton[k].click();
-                                    }
-                                }
-                                setTimeout(function () {
-                                    removeUrlJs((index + 1), urlList, true);
-                                }, 500);
-                            }
-                            else if (!submitButtonFound) {
-                                failedLinks.push(urlList[index]);
-                                console.log('Failed');
-                                const closeButton = document.querySelectorAll('.CwaK9 .RveJvd.snByac');
-                                for (let k = 0; k < closeButton.length; k++) {
-                                    if ((closeButton[k].childNodes[0] && (closeButton[k].childNodes[0].textContent).toLowerCase() == 'close') || (closeButton[2].textContent.toLowerCase() == 'close')) {
-                                        closeButton[k].click();
-                                    }
-                                }
-                                setTimeout(function () {
-                                    removeUrlJs((index + 1), urlList, true);
-                                }, 500);
-                            }
-                            else {
-                                submittedLinks.push(urlList[index]);
-                                console.log("Submitted");
-                                time = setInterval(function () {
-                                    if (document.querySelectorAll('.Mh0NNb.VcC8Fc').length > 0) {
-                                        const submittedText = 0;
-                                        if (document.querySelectorAll('.Mh0NNb.VcC8Fc')[0].childNodes[0].childNodes[0].textContent.toLowerCase() == 'request submitted') {
-                                            clearInterval(time);
-                                            time = setInterval(function () {
-                                                if (document.querySelector('.RveJvd.snByac')) {
-                                                    setTimeout(function () {
-                                                        removeUrlJs((index + 1), urlList);
-                                                    }, 400);
-                                                    clearInterval(time);
-                                                }
-                                            }, 500);
-                                        }
-                                    }
-                                }, 500);
-                            }
-                        }
-            }
-        } else {
+                }, 1500)
+            } else {
                 alert(`Total submitted: ${submittedLinks.length}\nTotal failed: ${failedLinks.length}\nThe file with all the links was downloaded!`);
                 await downloadResults();
-                async function downloadResults() {
-                    const submittedLinksFormatted = submittedLinks.join('\n');
-                    const failedLinksFormatted = failedLinks.join('\n');
-                    const txtContent = `Submitted links: \n${submittedLinksFormatted}\n\nFailed links: \n${failedLinksFormatted}`
-                    const encodedUri = encodeURI(`data:text/plain;charset=utf-8,${txtContent}`);
-                    const link = document.createElement('a');
-                    link.setAttribute('href', encodedUri);
-                    link.setAttribute('download', 'results.txt');
-                    document.body.appendChild(link);
-                    link.click();
-                    document.body.removeChild(link);
-                    reload();
+            }
+        }
+
+        async function clickNextButton(nextButton) {
+            if (!nextButton){
+                document.querySelector('.RveJvd.snByac').click();
+            }
+        }
+
+        async function urlToSubmissionBar(urlList, index) {
+            const urlBarLabelIndex = 0;
+            const urlBarIndex = 0;
+            const urlBarTextboxIndex = 1;
+            const urlBarLabel = document.querySelectorAll('.Ufn6O.PPB5Hf')[urlBarLabelIndex];
+            if (urlBarLabel) {
+                const urlBar = urlBarLabel.childNodes[urlBarIndex].childNodes[urlBarTextboxIndex];
+                if (urlBar) {
+                    urlBar.value = urlList[index];
                 }
-                async function reload() {
-                    await downloadResults();
-                    location.reload();
+            }
+
+        }
+
+        async function submissionNextButton() {
+            const nextButton = document.querySelectorAll('.RDPZE');
+            const nextButtonSpan = 2;
+            for (let j = 0; j < nextButton.length; j++) {
+                if (nextButton[j].childNodes[nextButtonSpan]) {
+                    nextButton[j].removeAttribute('aria-disabled');
+                    nextButton[j].setAttribute('tabindex', 0);
+                    nextButton[j].childNodes[nextButtonSpan].click();
                 }
             }
         }
-        await removeUrlJs(0, urlList);
+
+        async function submitRequest(submitButtonFound) {
+            let closeButtonFound = false;
+            const submitButton = document.querySelectorAll('.CwaK9 .RveJvd.snByac');
+
+            for (let k = 0; k < submitButton.length; k++) {
+                console.log(submitButton)
+                console.log(k, closeButtonFound)
+                if (submitButton[k].textContent.toLowerCase() == 'submit request') {
+                    submitButton[k].click();
+                    console.log("breaking out")
+                    return true;
+                } else {
+                    closeButtonFound = submitButton[k].textContent.toLowerCase() == 'close' ? true : false;
+                    if (closeButtonFound) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        async function checkOutcome(urlList, index, submitButtonFound) {
+            if (document.querySelectorAll('.PNenzf').length > 0) {
+                failedLinks.push(urlList[index]);
+                console.log(`Failed, ${urlList[index]} already exists as a submitted request, duplicates are not permitted.`)
+                const closeButton = document.querySelectorAll('.CwaK9 .RveJvd.snByac');
+                for (let k = 0; k < closeButton.length; k++) {
+                    if ((closeButton[k].childNodes[0] && (closeButton[k].childNodes[0].textContent).toLowerCase() == 'close')) {
+                        closeButton[k].click();
+                    }
+                }
+            } else if (!submitButtonFound) {
+                failedLinks.push(urlList[index]);
+                console.log('Failed');
+                const closeButton = document.querySelectorAll('.CwaK9 .RveJvd.snByac');
+                for (let k = 0; k < closeButton.length; k++) {
+                    if ((closeButton[k].childNodes[0] && (closeButton[k].childNodes[0].textContent).toLowerCase() == 'close')) {
+                        closeButton[k].click();
+                    }
+                }
+            } else {
+                submittedLinks.push(urlList[index]);
+                console.log("Submitted");
+            }
+        }
+
+        async function downloadResults() {
+            const submittedLinksFormatted = submittedLinks.join('\n');
+            const failedLinksFormatted = failedLinks.join('\n');
+            const txtContent = `Submitted links: \n${submittedLinksFormatted}\n\nFailed links: \n${failedLinksFormatted}`
+            const encodedUri = encodeURI(`data:text/plain;charset=utf-8,${txtContent}`);
+            const link = document.createElement('a');
+            link.setAttribute('href', encodedUri);
+            link.setAttribute('download', 'results.txt');
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+
+        async function reload() {
+            location.reload();
+        }
+
+        await removeUrlJs(0, urlListTrimmed);
     } catch (error) {
         console.error('An error occurred:', error);
     }
